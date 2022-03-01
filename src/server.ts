7ac6560133515a4e1e3e73b9583552b6b1835fd0
Leonardo <leonardotrotmartins@gmail.com>
2022-03-01 15:54:35 -0300

    add postgres connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import 'reflect-metadata';
 import { router } from './routes'
 import swaggerUi from "swagger-ui-express"
 import swaggerFile from "./swagger.json"
+
+import "./database"
 
 const app = express()
 
